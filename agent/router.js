b6585e457f9a9cefaa990b3173be9a37457f4775
Mymlogic.official <mymlogic.official@gmail.com>
2026-07-11 21:23:35 -0700

    Update router.js

--- agent/router.js
+++ agent/router.js
@@ -4,6 +4,7 @@ import * as ImageSkill from "./skills/images.js";
 import * as VoiceSkill from "./skills/voice.js";
 import * as SystemSkill from "./skills/system.js";
 import * as AutoSkill from "./skills/autoskill.js";
+import * as DiagnosticsSkill from "./skills/diagnostics.js";
 
 export class AgentRouter {
   constructor(core) {
@@ -12,13 +13,14 @@ export class AgentRouter {
 
   async handle(input) {
     switch (input.type) {
-      case "file":   return FileSkill.run(this.core, input);
-      case "image":  return ImageSkill.run(this.core, input);
-      case "voice":  return VoiceSkill.run(this.core, input);
-      case "system": return SystemSkill.run(this.core, input);
-      case "code":   return CodeSkill.run(this.core, input);
-      case "autoskill": return AutoSkill.run(this.core, input);
-      default:       return this.core.think(input.text);
+      case "file":       return FileSkill.run(this.core, input);
+      case "image":      return ImageSkill.run(this.core, input);
+      case "voice":      return VoiceSkill.run(this.core, input);
+      case "system":     return SystemSkill.run(this.core, input);
+      case "code":       return CodeSkill.run(this.core, input);
+      case "autoskill":  return AutoSkill.run(this.core, input);
+      case "diagnostics":return DiagnosticsSkill.run(this.core, input);
+      default:           return this.core.think(input.text);
     }
   }
 }

42b81dbed7f0777b997dfec007335952c8280901
Mymlogic.official <mymlogic.official@gmail.com>
2026-07-11 20:01:55 -0700

    Update core.js

--- agent/core.js
+++ agent/core.js
@@ -1,1 +1,14 @@
 
+import { Engine } from "./engine.js";
+import { Environment } from "./environment.js";
+
+export class AgentCore {
+  constructor(config) {
+    this.engine = new Engine(config.engine);
+    this.environment = new Environment(config.agent);
+  }
+
+  async think(prompt, attachments = {}) {
+    return this.engine.send(prompt, attachments);
+  }
+}

9d4842668760fdb87f9fdca838ff73d3a8b5eb41
Mymlogic.official <mymlogic.official@gmail.com>
2026-07-11 20:07:47 -0700

    Update style.css

--- ui/style.css
+++ ui/style.css
@@ -1,1 +1,31 @@
 
+body {
+  background: #111;
+  color: #eee;
+  font-family: Arial;
+}
+
+#chat {
+  width: 600px;
+  margin: auto;
+  padding-top: 40px;
+}
+
+#messages {
+  height: 400px;
+  overflow-y: scroll;
+  background: #222;
+  padding: 10px;
+  border-radius: 8px;
+}
+
+#input {
+  width: 100%;
+  height: 80px;
+  margin-top: 10px;
+}
+
+button {
+  margin-top: 10px;
+  width: 100%;
+}
